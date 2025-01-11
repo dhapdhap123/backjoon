0c8ae8f447f1e68f37676713ab6fe4e5ab2c1344
이영찬 <110439250+dhapdhap123@users.noreply.github.com>
2025-01-11 10:30:24 +0900

    이코테<정렬> 두배열원소교체

---
+++ 이것이코딩테스트다/6. 정렬/3.두배열원소교체.py
@@ -0,0 +1,11 @@
+n, k = list(map(int, input().split()))
+array_a = list(map(int, input().split()))
+array_b = list(map(int, input().split()))
+
+array_a.sort()
+array_b.sort(reverse=True)
+
+for i in range(k):
+    if (array_a[i] <= array_b[-i]):
+        array_a[i], array_b[i] = array_b[i], array_a[i]
+print(sum(array_a))
