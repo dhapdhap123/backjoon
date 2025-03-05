e287c253f81ded3fa63b49cb2d3a8dce569ada95
이영찬 <110439250+dhapdhap123@users.noreply.github.com>
2025-03-05 21:58:02 +0900

    구현: 5430

--- 구현/5430.py
+++ 구현/5430.py
@@ -4,29 +4,60 @@ from collections import deque
 
 T = int(input())
 
-def get_result(ord):
-    for i in range(len(orders)):
-        if orders[i] == 'R':
-            ord = -1 * ord
-        if orders[i] == 'D':
-            if len(nums) == 0:
-                return "error"            
+# 함수로 구현
+# def get_result():
+#     for i in range(len(orders)):
+#         if orders[i] == 'R':
+#             ord = -1 * ord
+#         if orders[i] == 'D':
+#             if len(nums) == 0:
+#                 return "error"            
             
-            if ord == 1:
-                nums.popleft()
-            elif ord == -1:
-                nums.pop()
-    if ord == -1:
-        nums.reverse()
-    return f"[{','.join(list(nums))}]"
+#             if ord == 1:
+#                 nums.popleft()
+#             elif ord == -1:
+#                 nums.pop()
+#     if ord == -1:
+#         nums.reverse()
+#     return f"[{','.join(list(nums))}]"
 
-for i in range(T):
+# for i in range(T):
+#     ord = 1
+#     orders = list(input().rstrip())
+#     n = int(input())
+#     str = input().rstrip()[1: -1]
+#     nums = []
+#     if str:
+#         nums = deque(str.split(','))
+#     result = get_result()
+#     print(result)
+
+# for 문으로 구현
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+T = int(input())
+
+for _ in range(T):
     ord = 1
     orders = list(input().rstrip())
     n = int(input())
-    str = input().rstrip()[1: -1]
-    nums = []
-    if str:
-        nums = deque(str.split(','))
-    result = get_result(ord)
-    print(result)
+    str = input().rstrip()[1:-1]
+    nums = deque(str.split(',')) if str else deque()
+
+    for command in orders:
+        if command == 'R':
+            ord *= -1
+        elif command == 'D':
+            if not nums:
+                print("error")
+                break
+            if ord == 1:
+                nums.popleft()
+            else:
+                nums.pop()
+    else:
+        if ord == -1:
+            nums.reverse()
+        print(f"[{','.join(nums)}]")
